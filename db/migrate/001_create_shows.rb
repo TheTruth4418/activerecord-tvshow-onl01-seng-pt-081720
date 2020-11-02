4c3bed366393d912d2ce29c9934ba161d84ae99c
William West <wwest466@gmail.com>
2020-11-02 18:52:59 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,9 @@
-create_table :shows do |x|
-  x.string :name
-  x.string :network
-  x.string :day
-  x.integer :rating
-end
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |x|
+      x.string :name
+      x.string :network
+      x.string :day
+      x.integer :rating
+    end
+  end
